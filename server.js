b236effd3cece74142177ab3302332d0622a8b73
samrat <samratshakya5@gmail.com>
2020-09-29 19:23:28 +0545

    fixes

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const express = require("express");
-const app = express();
 const port = process.env.PORT || 7000;
 //Server Static assets if in a production
 if (process.env.NODE_ENV === "production") {
